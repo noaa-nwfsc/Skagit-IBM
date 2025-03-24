b86ff06d1379fa1ea51074a6c17282bf546538c4
troyfrever <troyf@pobox.com>
2025-03-24 16:48:42 -0700

    add missing test file to git

---
+++ tests/hydro_model_test.cpp
@@ -0,0 +1,15 @@
+#include "hydro.h"
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE( "hydro updates timestep", "" ) {
+  std::vector<MapNode *> map;
+  std::vector<std::vector<float>> depths;
+  std::vector<std::vector<float>> temps;
+  float distFlow = 0;
+
+  HydroModel hydroModel = HydroModel(map, depths, temps, distFlow);
+  hydroModel.updateTime(4);
+  REQUIRE( hydroModel.getTime() == 4 );
+  hydroModel.updateTime(7);
+  REQUIRE( hydroModel.getTime() == 7 );
+}
